5cf52e0eaab1761cac0bd08ac25e46f8aa28a110
yama3ki <yama3ki.tada4@gmail.com>
2018-10-28 14:04:01 +0900

    2nd commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+const app = new Vue({
+    el: '#app',
+    template: '<h1>index</h1>'
+});
+
+console.log(app);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    entry: {
+        'main': './src/main.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        publicPath: '',
+        path: __dirname + '/public'
+    },
+    module: {
+        rules: [
+            { test: /\.vue$/, use: ['vue-loader'] },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.jpg$/, use: ['file-loader'] }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        alias: { 
+            'vue$': 'vue/dist/vue.esm.js',
+            'vuex$': 'vuex/dist/vuex.esm.js',
+            'vue-router$': 'vue-router/dist/vue-router.esm.js'
+        }
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    optimization: { minimize: false }
+};
