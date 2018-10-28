cf44f003b27993f29281f999729b66333e1709e6
yama3ki <yama3ki.tada4@gmail.com>
2018-10-28 15:11:53 +0900

    bootstrapを導入

---
+++ src/MainContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">bookshelf</a>
+        </nav>
+        <div class="row">
+            <div class="col-sm-2">
+                <p>left</p>
+            </div>
+            <div class="col-sm-10">
+                <p>main</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    components: {
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+import 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Vue from 'vue';
+import MainContainer from './MainContainer.vue';
 
 const app = new Vue({
     el: '#app',
-    template: '<h1>index</h1>'
+    template: '<main-container />',
+    components: {
+        MainContainer
+    }
 });
 
 console.log(app);
