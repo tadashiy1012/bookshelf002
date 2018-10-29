229b88b1ea7b9006a8e7f8558d8be4bab0cccd5a
yama3ki <yama3ki.tada4@gmail.com>
2018-10-29 18:37:58 +0900

    表示部分をちょっと作った

--- src/MainContainer.vue
+++ src/MainContainer.vue
@@ -1,21 +1,70 @@
 <template>
-    <div class="container">
+    <div class="container rootContainer">
         <nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="#">bookshelf</a>
+            <a class="navbar-brand" href="#">Navbar</a>
         </nav>
-        <div class="row">
-            <div class="col-sm-2">
-                <p>left</p>
+        <div class="row innerContainer">
+            <div class="col-sm-2 side">
+                <div class="sideContentConteiner">
+                    <p>left</p>
+                </div>
             </div>
-            <div class="col-sm-10">
-                <p>main</p>
+            <div class="col-sm-10 main">
+                <div :class="'mainContentContainer ' + (subShow ? 'mainContentContainer-grid' : 'mainContentContainer-block')">
+                    <div class="mainContentMain">
+                        <p>main</p>
+                        <button type="button" class="btn btn-primary" v-on:click="onToggleSub">toggle sub</button>
+                    </div>
+                    <div :class="'mainContentSub ' + (subShow ? '' : 'mainContentSub-hide')">
+                        <p>sub</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-    components: {
+    data: function() {
+        return {
+            subShow: false
+        };
+    },
+    methods: {
+        onToggleSub: function() {
+            this.subShow = !this.subShow;
+        }
     }
 }
 </script>
+<style scoped>
+.rootContainer {
+    height: 100%;
+}
+.innerContainer {
+    height: calc(100% - 56px);
+}
+.sideContentConteiner, .mainContentContainer {
+    margin: 0px 8px;
+}
+.mainContentContainer {
+    height: 100%;
+}
+.mainContentContainer-block {
+    display: block;
+}
+.mainContentContainer-grid {
+    display: grid;
+    grid-template-rows: auto auto;
+}
+.mainContentMain {
+    min-height: 200px;
+}
+.mainContentSub {
+    overflow-y: auto;
+    min-height: 200px;
+}
+.mainContentSub-hide {
+    display: none;
+}
+</style>
